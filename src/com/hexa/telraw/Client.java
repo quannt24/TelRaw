cf325511b57c09bebefbf4342db8454816aefbf0
Quan T. Nguyen <quan.nguyen@eastgate-software.com>
2017-09-01 18:32:39 +0700

    Handle connect/disconnect.

--- src/com/hexa/telraw/Client.java
+++ src/com/hexa/telraw/Client.java
@@ -3,45 +3,57 @@ package com.hexa.telraw;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 class Attachment {
-
 }
 
 public class Client implements CompletionHandler<Void, Attachment> {
 
-    private class ReadHandler implements CompletionHandler<Long, Attachment> {
+    public static int BUF_READ_SIZE = 1024;
+
+    private class Reader extends Thread {
+        private boolean running;
 
         @Override
-        public void completed(Long result, Attachment attachment) {
-            // TODO Auto-generated method stub
+        public void run() {
+            if (channel == null)
+                return;
+            ByteBuffer buf = ByteBuffer.allocate(BUF_READ_SIZE);
+            int n;
 
+            running = true;
+            while (running) {
+                buf.clear();
+                Future<Integer> ret = channel.read(buf);
+
+                try {
+                    n = ret.get();
+                    System.err.println("Client.Reader: read " + n + " bytes\n");
+
+                    if (n > 0) {
+                        byte[] raw = new byte[n];
+                        buf.get(raw);
+                        /*
+                         * String str = new String(raw, StandardCharsets.UTF_8);
+                         * System.err.println("Buffer: [" + str + "]\n");
+                         */
+                        if (dataObs != null)
+                            dataObs.onReceive(raw);
+                    }
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
         }
-
-        @Override
-        public void failed(Throwable exc, Attachment attachment) {
-            // TODO Auto-generated method stub
-
-        }
-
-    }
-
-    private class WriteHandler implements CompletionHandler<Integer, Attachment> {
-
-        @Override
-        public void completed(Integer result, Attachment attachment) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void failed(Throwable exc, Attachment attachment) {
-            // TODO Auto-generated method stub
-
-        }
-
     }
 
     private String host;
@@ -50,23 +62,24 @@ public class Client implements CompletionHandler<Void, Attachment> {
     private boolean connected;
     private ConnectionObserver conObs;
     private DataObserver dataObs;
+    private Reader reader;
 
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
         connected = false;
     }
-    
+
     public boolean isConnected() {
         return connected;
     }
 
-    public void setDataObserver(DataObserver obs) {
-        this.dataObs = obs;
-    }
-
     public void setConnectionObserver(ConnectionObserver obs) {
         this.conObs = obs;
+    }
+
+    public void setDataObserver(DataObserver obs) {
+        this.dataObs = obs;
     }
 
     public void connect() throws IOException {
@@ -75,22 +88,44 @@ public class Client implements CompletionHandler<Void, Attachment> {
         channel.connect(serverAddr, null, this);
     }
 
-    public void disconnect() {
+    public void disconnect() throws IOException {
+        if (reader != null)
+            reader.running = false; // Stop reading thread
+        if (channel != null)
+            channel.close();
+
+        try {
+            reader.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        conObs.onDisconnected();
     }
 
     public void send(String str) {
+        ByteBuffer buf = ByteBuffer.wrap(str.getBytes());
+        channel.write(buf);
     }
 
     @Override
     public void completed(Void result, Attachment attachment) {
         connected = true;
-        if (conObs != null) conObs.onConnected();
+        if (conObs != null)
+            conObs.onConnected();
+        // Start reading
+        if (reader == null) {
+            reader = new Reader();
+            reader.start();
+        }
     }
 
     @Override
     public void failed(Throwable exc, Attachment attachment) {
         connected = false;
-        if (conObs != null) conObs.onConnectionFailure();
+        if (conObs != null)
+            conObs.onConnectionFailure();
     }
 
 }
